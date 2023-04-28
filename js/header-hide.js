ac669e186e55b7f628ff81055a8a18aa2917101b
amulll <a2233556651@gmail.com>
2023-04-28 16:49:27 +0800

    '0428-2'

---
+++ js/header-hide.js
@@ -0,0 +1,15 @@
+const header = document.querySelector('web-header');
+const headerInitialHeight = header.offsetHeight;
+let lastScrollPosition = 0;
+
+window.addEventListener('scroll', function() {
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > lastScrollPosition) { // 向下滾動
+    header.style.opacity = '0';;
+  } else { // 向上滾動
+    header.style.opacity = '1';;
+  }
+
+  lastScrollPosition = scrollPosition;
+});
