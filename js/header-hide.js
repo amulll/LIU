2b40d1b3265856bfd8fb211714d658843d8a5c31
amulll <a2233556651@gmail.com>
2023-05-02 14:40:04 +0800

    '0502'

--- js/header-hide.js
+++ js/header-hide.js
@@ -5,7 +5,7 @@ let lastScrollPosition = 0;
 window.addEventListener('scroll', function() {
   const scrollPosition = window.scrollY;
 
-  if (scrollPosition > lastScrollPosition) { // 向下滾動
+  if (scrollPosition > 100 && scrollPosition > lastScrollPosition) { // 向下滾動
     header.style.opacity = '0';;
   } else { // 向上滾動
     header.style.opacity = '1';;
